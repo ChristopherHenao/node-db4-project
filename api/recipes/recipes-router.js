0bdd0a6e3ba878bd7c1c386bc0811a811f89fe4c
ChristoherHenao <cq.henao@gmail.com>
2021-09-09 22:08:13 -0400

    wip

--- api/recipes/recipes-router.js
+++ api/recipes/recipes-router.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const Recipes = require('./recipes-model')
 
 const router = express.Router()
 
-router.get('/', (req, res, next) => {
-    res.json('get is working')
+router.get('/:recipe_id', async (req, res, next) => {
+    try {
+        const recipe = await Recipes.getRecipeById(req.params.recipe_id)
+        if (!recipe) {
+            next({
+                status: 404,
+                message: 'recipe not found'
+            })
+        }
+        else {
+            res.json(recipe)
+        }
+    }
+    catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,23 @@
+const sharedConfig = {
+  client: 'sqlite3',
+  migrations: { directory: './data/migrations'},
+  useNullAsDefault: true,
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)}
+  }
+}
+
 module.exports = {
-    development: {
-      client: 'sqlite3',
-      useNullAsDefault: true,
-      connection: {
-        filename: './data/recipes.db3',
-      },
-      migrations: {
-        directory: './data/migrations'
-      },
-      seeds: {
-        directory: './data/seeds'
-      },
-      pool: {
-        afterCreate: (conn, done) => {
-          conn.run('PRAGMA foreign_keys = ON', done);
-        },
-      },
-    },
-  };
+  development: {
+    ...sharedConfig,
+    connection: { filename: './data/recipes.db3' },
+    seeds: { directory: './data/seeds' },
+  },
+  testing: {
+    ...sharedConfig,
+    connection: { filename: './data/recipes.test.db3' },
+  },
+  production: {}
+}
+
